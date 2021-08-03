dfc7a5b30a0b47cd9e82896343012976cf9261f1
[AndreaS] <solfa_90@hotmail.it>
2021-08-03 16:35:10 +0200

    get user input with hook useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
+import TodoList from './components/TodoList'
+import NewTodo from './components/NewTodo'
 
-function App() {
+
+const App: React.FC = () => {
+  const todos = [{id: 't1', text: 'Finish the course'}];
+
   return (
     <div className="App">
-
+      <NewTodo />
+      <TodoList todos={todos} />
     </div>
   );
 }
